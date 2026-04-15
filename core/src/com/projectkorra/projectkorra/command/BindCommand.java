329f64c03200df867eb93bd381d6e5f413db4bde
Unprankable01 <25pcurtis@gmail.com>
2026-04-15 01:33:29 -0600

    Remove can bind debug log

--- core/src/com/projectkorra/projectkorra/command/BindCommand.java
+++ core/src/com/projectkorra/projectkorra/command/BindCommand.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.util.ChatUtil;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
@@ -86,7 +85,6 @@ public class BindCommand extends PKCommand {
 
 		final BendingPlayer bPlayer = BendingPlayer.getBendingPlayer((Player) sender);
 		final CoreAbility coreAbil = CoreAbility.getAbility(ability);
-        ProjectKorra.log.info("canBind: " + bPlayer.canBind(coreAbil));
 		if (bPlayer == null) {
 			ChatUtil.sendBrandingMessage(sender, ChatColor.RED + this.loadingInfo);
 			return;
